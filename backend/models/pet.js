d706fbc6f3938691d2e305c0228c4da29b759152
Daniel Wasewicz <daniel.wasewicz@gmail.com>
2017-11-28 15:24:50 -0600

    initial backend commit

---
+++ backend/models/pet.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose")
+
+var PetSchema = new mongoose.Schema({
+	name: {type:String default:null},
+	location: {type:String default:null},
+	description: {type:String default:null},
+	type: {type:String default:null},
+	breed: {type:String default:null},
+	color: {type:String default:null},
+	size: {type:String default:null},
+	gender: {type:String default:null},
+	datefound: {type:Date, default:null},
+	date: {type:Date, default:Date.now}
+})
+
+module.exports = mongoose.model('Pets', PetSchema)
