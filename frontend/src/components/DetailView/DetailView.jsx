82763bc94d4bb79d59051afc3961e8ac0de1915f
Daniel Wasewicz <daniel.wasewicz@gmail.com>
2017-12-07 18:31:42 -0600

    update

--- frontend/src/components/DetailView/DetailView.jsx
+++ frontend/src/components/DetailView/DetailView.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Button, Card, Icon } from 'semantic-ui-react'
 import styles from './styles.scss' 
 import axios from 'axios'
 
@@ -16,22 +17,27 @@ class DetailView extends Component{
     }
 
     componentWillMount(){
-    	axios.get('/api/pets/'+ this.this.props.match.params.id).then( (res) => {
-            console.log(res);
-            
+    	axios.get('/api/pets/'+ this.props.match.params.id).then( (res) => {
+            console.log(res.data.data);
+            this.setState({
+            	pet_id: this.props.match.params.id,
+            	pet_data: res.data.data
+            })
         })
     }
 
 	render(){
 		return(
 			<div className="DetailView">
-				<h1>Hello</h1>
+				<span className="GroupTitle">
+	            <h1><Icon name="paw" />Pet Finder</h1>
+	            </span>
 				<div className="PetInformation">
-					<PetInformation id={this.state.pet_id}/>
+					<PetInformation id={this.state.pet_id} data={this.state.pet_data}/>
 				</div>
 
 				<div className="CommentList">
-					<CommentList id={this.state.pet_id}/>
+					<CommentList id={this.state.pet_id} data={this.state.pet_data.comments}/>
 				</div>
 
 			</div>
@@ -45,6 +51,16 @@ class PetInformation extends Component{
 	render(){
 		return(
 			<div>
+		        <Card key={this.props.data._id}>
+		        	<span>
+		        		<img src={this.props.data.img_url}/> 
+	    				<h2 >{this.props.data.name}</h2>
+	    				<h2 >{this.props.data.type}</h2>
+	    				<h2 >{this.props.data.location}</h2>
+	    				<h2 >{this.props.data.datefound}</h2>
+	    				<h2 >{this.props.data.description}</h2>
+    				</span>
+    			</Card>
 			</div>
 		)
 	}
